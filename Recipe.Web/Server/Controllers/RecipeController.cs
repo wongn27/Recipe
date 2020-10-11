9f46402a99c711f594cb99de0b3ba2ed88055f60
Natalie Wong <wongn341@gmail.com>
2020-10-11 02:59:44 -0400

    Add recipe controller

---
+++ Recipe.Web/Server/Controllers/RecipeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Recipe.Web.Data;
+
+namespace Recipe.Web.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipeController : ControllerBase
+    {
+        private readonly ILogger<RecipeController> logger;
+        private readonly RecipeContext context;
+
+        public RecipeController(ILogger<RecipeController> logger, RecipeContext context)
+        {
+            this.logger = logger;
+            this.context = context;
+        }
+    }
+}
