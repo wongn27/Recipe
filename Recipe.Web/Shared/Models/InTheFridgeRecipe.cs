d963a2fd2fd8d197d83e453fea0b7c080a95a8f2
Natalie Wong <wongn341@gmail.com>
2020-10-11 02:55:50 -0400

    Rename RecipeClasses to SpoonacularRecipe Move Recipe class into InTheFridgeRecipe class Create RecipeApiSourceEnum

---
+++ Recipe.Web/Shared/Models/InTheFridgeRecipe.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Recipe.Web.Data.Enums;
+
+namespace Recipe.Web.Data.Models
+{
+    public class InTheFridgeRecipe
+    {
+        public Guid Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(100)]
+        public string CookingTime { get; set; }
+
+        public string PictureUrl { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+
+        public RecipeApiSource ApiSourceName { get; set; }
+        public string ApiJson { get; set; }
+    }
+}
