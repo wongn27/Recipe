6b86d87aeeae93f8c4fdedef32f6984c77f26cc9
Natalie Wong <wongn341@gmail.com>
2020-10-11 02:52:56 -0400

    Rename users tables username to email create InTheFridgeRecipe table names Recipes

---
+++ Recipe.Web/Shared/Migrations/20201011032619_Create_InTheFridgeRecipe_Table.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recipe.Web.Data.Migrations
+{
+    public partial class Create_InTheFridgeRecipe_Table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: true),
+                    CookingTime = table.Column<string>(maxLength: 100, nullable: true),
+                    PictureUrl = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Ingredients = table.Column<string>(nullable: true),
+                    Steps = table.Column<string>(nullable: true),
+                    ApiSourceName = table.Column<int>(nullable: false),
+                    ApiJson = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipes", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipes");
+        }
+    }
+}
