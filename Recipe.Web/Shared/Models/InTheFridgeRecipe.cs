a2678fb4bdc867e369ee97d46f274f9aba1d923b
Natalie Wong <wongn341@gmail.com>
2020-10-24 00:21:52 -0400

    Added recipe columns that are needed.

--- Recipe.Web/Shared/Models/InTheFridgeRecipe.cs
+++ Recipe.Web/Shared/Models/InTheFridgeRecipe.cs
@@ -10,12 +10,24 @@ namespace Recipe.Web.Data.Models
         public Guid Id { get; set; }
         [StringLength(100)]
         public string Name { get; set; }
+        public string Description { get; set; }
         [StringLength(100)]
         public string CookingTime { get; set; }
-
         public string PictureUrl { get; set; }
-        public string Description { get; set; }
+        public string Cuisines { get; set; }
         public string Ingredients { get; set; }
+        public string Instructions { get; set; }
         public string Steps { get; set; }
+        public int CalorieCount { get; set; }
+        public int Servings { get; set; }
+        public bool IsVegan { get; set; }
+        public bool IsVegetarian { get; set; }
+        public bool IsGlutenFree { get; set; }
+        public bool IsDairyFree { get; set; }
+        public bool IsPopular { get; set; }
+        public bool IsCheap { get; set; }
+        public bool IsHealthy { get; set; }
+        public bool IsSustainable { get; set; }
+
     }
 }
