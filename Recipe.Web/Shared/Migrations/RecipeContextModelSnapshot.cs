7c6ee6f9a176fa363b14ff3d600579e5bc99e7f2
Natalie Wong <wongn341@gmail.com>
2020-10-24 00:12:11 -0400

    context changes

--- Recipe.Web/Shared/Migrations/RecipeContextModelSnapshot.cs
+++ Recipe.Web/Shared/Migrations/RecipeContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace Recipe.Web.Data.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.8")
+                .HasAnnotation("ProductVersion", "3.1.9")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -24,12 +24,6 @@ namespace Recipe.Web.Data.Migrations
                     b.Property<Guid>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier");
-
-                    b.Property<string>("ApiJson")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<int>("ApiSourceName")
-                        .HasColumnType("int");
 
                     b.Property<string>("CookingTime")
                         .HasColumnType("nvarchar(100)")
@@ -62,6 +56,10 @@ namespace Recipe.Web.Data.Migrations
                         .ValueGeneratedOnAdd()
                         .HasColumnType("uniqueidentifier");
 
+                    b.Property<string>("Bio")
+                        .HasColumnType("nvarchar(100)")
+                        .HasMaxLength(100);
+
                     b.Property<string>("Email")
                         .IsRequired()
                         .HasColumnType("nvarchar(50)")
@@ -70,13 +68,26 @@ namespace Recipe.Web.Data.Migrations
                     b.Property<int>("FailedLoginAttempts")
                         .HasColumnType("int");
 
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(30)")
+                        .HasMaxLength(30);
+
                     b.Property<bool>("IsLocked")
                         .HasColumnType("bit");
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(30)")
+                        .HasMaxLength(30);
 
                     b.Property<string>("Password")
                         .IsRequired()
                         .HasColumnType("nvarchar(50)")
                         .HasMaxLength(50);
+
+                    b.Property<byte[]>("ProfilePicture")
+                        .HasColumnType("varbinary(max)");
 
                     b.Property<string>("SecurityAnswer1")
                         .IsRequired()
--- Recipe.Web/Shared/RecipeContext.cs
+++ Recipe.Web/Shared/RecipeContext.cs
@@ -5,8 +5,6 @@ namespace Recipe.Web.Data
 {
     public class RecipeContext : DbContext
     {
-        private string password { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=tcp:recipe-fsc.database.windows.net,1433;Initial Catalog=recipe;Persist Security Info=False;User ID=wongnatmei@gmail.com@recipe-fsc;Password=Twocashews$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
