a5a2e00130574dadf4291d188bc2c1967a6acb07
Ryan <ruytam@gmail.com>
2020-11-09 11:06:20 +0800

    Homepage

---
+++ Recipe.Web/Shared/Models/RecipeTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recipe.Web.Data.Models
+{
+
+
+    public class RootobjectRecipe
+    {
+        public RecipeTest[] results { get; set; }
+        public int offset { get; set; }
+        public int number { get; set; }
+        public int totalResults { get; set; }
+    }
+
+    public class RecipeTest
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string image { get; set; }
+        public string imageType { get; set; }
+    }
+
+
+}
