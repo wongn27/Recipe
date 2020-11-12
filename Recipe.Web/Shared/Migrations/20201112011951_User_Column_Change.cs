07efcba407828006fc508dcf0478b64dac371775
Natalie Wong <wongn341@gmail.com>
2020-11-11 20:24:42 -0500

    Added migrations

---
+++ Recipe.Web/Shared/Migrations/20201112011951_User_Column_Change.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Recipe.Web.Data.Migrations
+{
+    public partial class User_Column_Change : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfilePicture",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "ProfilePicture",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
