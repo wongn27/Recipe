f99d3118f53d92f01a1e7956f840bf1dd8bf2035
Natalie Wong <wongn341@gmail.com>
2020-12-16 00:26:38 -0500

    rename class, add extension method for checking if recipe and spoon recipe are same

---
+++ Recipe.Web/Shared/Extensions/InTheFridgeRecipeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Recipe.Web.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recipe.Web.Data.Extensions
+{
+    public static class InTheFridgeRecipeExtensions
+    {
+        public static bool IsTheSameRecipe(this InTheFridgeRecipe recipe, SpoonacularRecipe spoonRecipe)
+        {
+            // criteria for being the same recipe - Name, and desc
+            return recipe.Name.Equals(spoonRecipe.Title, StringComparison.OrdinalIgnoreCase) &&
+                   recipe.Description.Equals(spoonRecipe.Summary, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
