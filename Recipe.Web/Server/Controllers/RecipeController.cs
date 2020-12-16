9b21a64b26e7aeae5ad76f2ac0ebe6f59f7f7534
Natalie Wong <wongn341@gmail.com>
2020-12-16 00:24:20 -0500

    Make recipe search combine api results and database recipe results distincly for rendering and automatically adding recipes not in the database.

--- Recipe.Web/Server/Controllers/RecipeController.cs
+++ Recipe.Web/Server/Controllers/RecipeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -9,9 +10,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Recipe.Web.Data;
+using Recipe.Web.Data.Extensions;
 using Recipe.Web.Data.Models;
 using Recipe.Web.Data.Utilities;
 using Recipe.Web.Server.Services;
+using MoreLinq;
 
 namespace Recipe.Web.Server.Controllers
 {
@@ -31,19 +34,76 @@ namespace Recipe.Web.Server.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> SearchForRecipes(string apiRoute)
+        public async Task<ActionResult<InTheFridgeRecipe[]>> SearchForRecipes(SpoonacularRecipeSearchModel searchModel)
         {
-            var spoonacularRecipes = await httpClient.GetFromJsonAsync<SpoonacularRecipe[]>(apiRoute);
+            // Create endpoint builder and add parameters for the complex search endpoint for bulk recipe data
+            var endpointBuilder = new SpoonacularApiEndpointBuilder(SpoonacularApiEndpoint.ComplexSearch);
+            endpointBuilder.AddParameter<string>("query", searchModel.SearchString);
+            endpointBuilder.AddParameter<int>("number", searchModel.NumberOfResults);
+
+            // If any filters were selected, add to list of intolerences
+            var listOfIntolerances = new List<string>();
+
+            if (searchModel.IsDairyFree)
+                listOfIntolerances.Add("Dairy");
+
+            if (searchModel.IsGlutenFree)
+                listOfIntolerances.Add("Gluten");
+
+            if (searchModel.IsVegetarian)
+                listOfIntolerances.Add("Vegetarian");
+
+            // Use AddParameters function to add complex multi-value parameter with a specific delimiter
+            // i.e. ...&intolerances=Dairy,Gluten,Vegetarian&...
+            if (listOfIntolerances.Any())
+                endpointBuilder.AddParameters("intolerances", ",", listOfIntolerances.ToArray());
+
+            // Build endpoint and get a spoonacular response from complex search api endpoint
+            var apiRoute = endpointBuilder.BuildEndpoint();
+            var spoonacularResponse = await httpClient.GetFromJsonAsync<SpoonacularRecipeComplexSearchResponse>(apiRoute);
+
+            // Get all the recipe ids from complex query response in preperation for Bulk Recipes api endpoint request
+            var recipeIds = spoonacularResponse.Results.Select(e => e.Id).ToArray();
+            SpoonacularRecipe[] spoonRecipes = null;
+
+            // If any recipes are found, request the bulk recipes endpoint using the ids gathered from above
+            if (recipeIds.Any())
+            {
+                // Use AddParameters function to add complex multi-value parameter with a specific delimiter
+                // i.e. ...&ids=101,102,103&...
+                endpointBuilder = new SpoonacularApiEndpointBuilder(SpoonacularApiEndpoint.BulkRecipeByIds);
+                endpointBuilder.AddParameters("ids", ",", recipeIds);
+                var apiRouteRecipes = endpointBuilder.BuildEndpoint();
+                spoonRecipes = await httpClient.GetFromJsonAsync<SpoonacularRecipe[]>(apiRouteRecipes);
+            }
+
+            // Gather recipes stored in the recipes table that contains the search query in the name and description of the recipe.
+            var inTheFridgeRecipes = context.Recipes
+                .Where(r => r.Name.ToLower().Contains(searchModel.SearchString.ToLower()) ||
+                            r.Description.ToLower().Contains(searchModel.SearchString.ToLower()))
+                .ToList();
+
+            // Map spoonacular recipes to InTheFridgeRecipes.
             var mappingService = new RecipeMappingService();
-            var recipes = mappingService.MapTo(spoonacularRecipes);
+            var mappedRecipes = mappingService.MapTo(spoonRecipes).ToList();
 
-            if (!recipes.Any())
-                return null;
+            // If there are any recipes in the database, lets remove some from api results so we dont display the same recipe twice.
+            if (inTheFridgeRecipes.Any() || mappedRecipes.Any())
+            {
+                var allRecipes = inTheFridgeRecipes.Concat(mappedRecipes).ToList();
+                var recipesToRender = allRecipes.DistinctBy(e => e.Name).DistinctBy(e => e.Description).ToList();
+                var distinctRecipes = allRecipes.ExceptBy(recipesToRender, e => e.Description).ToList();
 
-            context.Recipes.AddRange(recipes);
-            context.SaveChanges();
+                if (distinctRecipes.Any())
+                {
+                    context.Recipes.AddRange(distinctRecipes);
+                    context.SaveChanges();
+                }
 
-            return Ok();
+                return Ok(recipesToRender);
+            }
+
+            return NoContent();
         }
 
         private async Task<SpoonacularRecipe[]> GetApiResponseFromSpoonacular(string searchString)
